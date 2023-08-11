c884b2b210905bcb56f0501c240b8d45c3523914
tranhongson2943 <sonth@viit.com.vn>
2023-08-11 10:07:28 +0700

    making header

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,54 @@
 <template>
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+  <img src="https://demo.wpenjoy.com/visualmag-pro/wp-content/uploads/sites/55/2023/02/logo-1-300x56.png" width="220" height="41.06" alt="">
+</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 </template>
 
+
+
 <script>
-export default {
-}
+export default {};
 </script>
+<style>
+nav {
+  width: 100%;
+  height: 76px;
+  color: white;
+}
+.ul {
+  display: flex;
+}
+</style>
--- pages/visualmag-pro.vue
+++ pages/visualmag-pro.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
+  <div class="b-container-fluid">
     <Header/>
-    header
     <NuxtChild/>
   </div>
 
@@ -12,3 +11,5 @@
 export default {
 }
 </script>
+<style>
+</style>
